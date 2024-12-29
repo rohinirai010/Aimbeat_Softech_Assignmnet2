7d3f9b21e2546b386b7d5576524b0166b5182056
rohinirai010 <rairohini2001@gmail.com>
2024-12-29 18:04:41 +0530

    Configure GitHub Pages deployment with vite-plugin-gh-pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { ghPages } from 'vite-plugin-gh-pages';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    ghPages({
+      branch: 'gh-pages',
+    }),
+  ],
+  base: '/Aimbeat_Softech_Assignmnet2/', 
+});
