34841e0ad00c1aca62d85c525331dca158a4ab16
rohinirai010 <rairohini2001@gmail.com>
2024-12-29 18:23:34 +0530

    Minor Changes

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import ghPages from 'vite-plugin-gh-pages';
+import { ghPages } from 'vite-plugin-gh-pages';  
 
 export default defineConfig({
   plugins: [
     react(),
-    ghPages(), 
+    ghPages(),  
   ],
   base: '/Aimbeat_Softech_Assignmnet2/',  
 });
